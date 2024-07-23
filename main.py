831897bbe1766796164b453ac610f79435841cd4
Tankerch <mukmin.lc@gmail.com>
2024-07-23 16:46:16 +0700

    Update 20240723
    1. Add dir check
    2. Integrated pdf2docx with observer event

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
+import os
 import pdf2docx
 import watchdog.observers
 import watchdog.events
 
-pdf_path = './pdf/Faktur Dinasty - INV_2024_00440.pdf'
-docx_path = './docx/testing.docx'
+
+def checkRequiredDir():
+    if not os.path.exists("./docx"):
+        os.mkdir("./docx")
+    if not os.path.exists("./pdf"):
+        os.mkdir("./pdf")
+
+
+def convertPdf2Docx(src_path: str, dist_path: str):
+    pdf2docx.parse(pdf_file=src_path, docx_file=dist_path)
 
 
 class CustomFileHandler(watchdog.events.FileSystemEventHandler):
-    def dispatch(self, event: watchdog.events.FileSystemEvent) -> None:
-        print(event)
+    def dispatch(self, event) -> None:
+        if event.event_type == "created" or event.event_type == "modified":
+            filename = os.path.splitext(os.path.basename(event.src_path))[0]
+            dist_path = os.path.join('./docx/', f'{filename}.docx')
+            convertPdf2Docx(src_path=event.src_path, dist_path=dist_path)
         return super().dispatch(event)
 
 
@@ -16,8 +28,9 @@ def main():
     observer = watchdog.observers.Observer()
     handler = CustomFileHandler()
 
-    # pdf2docx.parse(pdf_file=pdf_path, docx_file=docx_path)
-    observer.schedule(event_handler=handler, path="./pdf")
+    checkRequiredDir()
+
+    observer.schedule(event_handler=handler, path=os.path.normpath("./pdf"))
     observer.start()
     try:
         while observer.is_alive():
@@ -25,6 +38,7 @@ def main():
     finally:
         observer.stop()
         observer.join()
+
 
 if __name__ == "__main__":
     main()
